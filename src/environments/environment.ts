b0611a6f7951e0bfb776e6d22b0a3cff11898cc1
armanali485 <armanali485.hitcsecs2020@gmail.com>
2025-07-09 19:22:16 +0530

    Attendance System Frontend

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    // baseURL : "https://us-central1-attendance-system-192fb.cloudfunctions.net/attendanceSystem/api/"
+    baseURL : "https://attendancesystembackend.onrender.com/api/"
+  };
